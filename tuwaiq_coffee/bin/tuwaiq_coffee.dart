2a95a38709214fe7b527069b2eba2de54cc8542e
Najwa-aldahri <aldahri.najwa@gmail.com>
2024-08-01 14:49:47 +0300

    menu

--- tuwaiq_coffee/bin/tuwaiq_coffee.dart
+++ tuwaiq_coffee/bin/tuwaiq_coffee.dart
@@ -1,5 +1,26 @@
-import 'package:tuwaiq_coffee/tuwaiq_coffee.dart' as tuwaiq_coffee;
+import 'dart:io';
+import 'menu.dart';
+// import 'package:tuwaiq_coffee/tuwaiq_coffee.dart' as tuwaiq_coffee;
 
 void main(List<String> arguments) {
-  print('Hello world: ${tuwaiq_coffee.calculate()}!');
+  print("-------------------------");
+  print("Welcome to coffe system");
+  print("-------------------------");
+  print("I am your paresta for today how can i help you?");
+  print("what is your name?");
+  String? username = stdin.readLineSync();
+  print("Hello $username nice to meet you");
+  print("1-new order");
+  print("2-Display my order");
+  int? input = int.parse(stdin.readLineSync()!);
+
+  // Create an instance of CoffeeShopMenu
+  Menu menu = Menu();
+
+  switch (input) {
+    case 1:
+      menu.myMenu();
+  }
+
+// class Menu{
 }
