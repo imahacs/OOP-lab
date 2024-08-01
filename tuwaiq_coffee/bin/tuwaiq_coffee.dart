d3eeb08f03edaec3623b0953858f68fa1edf6f1f
MAHA Al-ATAWI <mem880.a@gmail.com>
2024-08-01 14:01:54 +0300

    Dart files

---
+++ tuwaiq_coffee/bin/tuwaiq_coffee.dart
@@ -0,0 +1,5 @@
+import 'package:tuwaiq_coffee/tuwaiq_coffee.dart' as tuwaiq_coffee;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tuwaiq_coffee.calculate()}!');
+}
---
+++ tuwaiq_coffee/test/tuwaiq_coffee_test.dart
@@ -0,0 +1,8 @@
+import 'package:tuwaiq_coffee/tuwaiq_coffee.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
